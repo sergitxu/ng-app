4a8e13687f2aa5e50f9ecd721045e9ce2e918a9e
Sergitxu <sergiortiz@gmail.com>
2019-04-17 23:42:41 +0200

    Get data from an external service :rocket:

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  sharedMethod() {
-    return console.log('patatuca');
+  sharedMethod(){
+    console.log('Launched a shared method!')
+  }
+
+  getData() {
+    return this.http.get('http://api.open-notify.org/astros.json');
   }
 }
