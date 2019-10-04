658f98624cb7f31066d6a0a90c762c1ab42044fa
Ortiz Hernández <SergioOrt@youforce.net>
2019-10-04 18:55:27 +0200

    header removed

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  appTitle : string = 'Ng App'
+  appTitle = 'Ng App';
 
   constructor() { }
 
